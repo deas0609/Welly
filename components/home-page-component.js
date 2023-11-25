8bff29c55e5fa5d0c91121bea9a85553d206f0e7
deas0609 <a9s2d0f1g8@gmail.com>
2023-11-25 16:10:45 +0800

    第二題、第三題完成

--- components/home-page-component.js
+++ components/home-page-component.js
@@ -6,6 +6,30 @@ export default function HomePageComponent() {
   const StringReverseFunction = (str) => {
     return str.split('').reverse().join('')
   }
+  // 第二題
+  const [arrayFiltering, setArrayFiltering] = useState('')
+  const arrayFilteringFunction = (arrayNumbers, filterValue) => {
+    const array = arrayNumbers.split(',')
+    const result = array.filter((number) => number > filterValue)
+
+    return result.join(', ')
+  }
+  //   第三題
+  const [lastName, setLastName] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const formatName = (lastName, firstName) => {
+    // let formattedName = ''
+    // if (firstName) {
+    //   formattedName += firstName
+    // }
+    // if (lastName) {
+    //   formattedName += ' ' + lastName
+    // }
+    // return formattedName
+    return `${firstName || ''}${firstName && lastName ? ' ' : ''}${
+      lastName || ''
+    }`
+  }
 
   return (
     <>
@@ -22,6 +46,42 @@ export default function HomePageComponent() {
           }}
         />
         <h3>{StringReverseFunction(stringReverse)}</h3>
+      </div>
+      {/* 第二題 */}
+      <div>
+        <h2>2.JavaScript: 陣列過濾</h2>
+        <input
+          type="text"
+          placeholder="輸入多組數字，並用','間隔開來"
+          value={arrayFiltering}
+          onChange={(e) => {
+            setArrayFiltering(e.target.value)
+          }}
+          size="30"
+        />
+
+        <h3>{arrayFilteringFunction(arrayFiltering, 5)}</h3>
+      </div>
+      {/* 第三題 */}
+      <div>
+        <h2>3.JavaScript: 重構</h2>
+        <input
+          type="text"
+          placeholder="輸入姓氏"
+          value={lastName}
+          onChange={(e) => {
+            setLastName(e.target.value)
+          }}
+        />
+        <input
+          type="text"
+          placeholder="輸入名稱"
+          value={firstName}
+          onChange={(e) => {
+            setFirstName(e.target.value)
+          }}
+        />
+        <h3>{formatName(lastName, firstName)}</h3>
       </div>
     </>
   )
